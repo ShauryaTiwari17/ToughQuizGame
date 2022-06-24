f5df69678509f49088a3c85f3e75523837521d57
Shaurya Tiwari <72653870+ShauryaTiwari17@users.noreply.github.com>
2022-06-24 15:24:30 +0530

    Add files via upload
    This is game contains just 2 questions but it is very tough!

---
+++ Game.py
@@ -0,0 +1,18 @@
+q1= input("which is the tallest statue in the world?\n").lower()
+
+if(q1=="statue of unity"):
+    print("Right!")
+else:
+    print("wrong")
+    
+q2= input("which country is smaller than Vatician City in this world?\n").lower()
+if(q2=="principality of sealand"):
+    print("Right")
+else:
+    print("Wrong u are an idiot!")
+
+
+
+
+
+
